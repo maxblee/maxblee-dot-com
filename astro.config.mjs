983dccf9459cd51dba3130681e1cb0442d92b358
Max Lee <maxbmhlee@gmail.com>
2023-09-01 08:32:36 -0400

    added eslint setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
-import { defineConfig } from "astro/config";
+import { defineConfig } from 'astro/config';
 
-import mdx from "@astrojs/mdx";
-import sitemap from "@astrojs/sitemap";
-import svelte from "@astrojs/svelte";
-import tailwind from "@astrojs/tailwind";
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import svelte from '@astrojs/svelte';
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte(), tailwind(), mdx(), sitemap()],
+	integrations: [svelte(), tailwind(), mdx(), sitemap()],
 });
