3fea0e1ced75ab791a3888db2e3a8e58b90697d7
Max Lee <maxbmhlee@gmail.com>
2023-08-31 21:05:18 -0400

    Added svelte, tailwind, mdx, and sitemap integrations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,11 @@
 import { defineConfig } from 'astro/config';
+import svelte from "@astrojs/svelte";
+
+import tailwind from "@astrojs/tailwind";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [svelte(), tailwind(), mdx(), sitemap()]
+});
