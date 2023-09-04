c14fc43b3bb7aa2ce5a3b25cde50b25340ee9363
Max Lee <maxbmhlee@gmail.com>
2023-09-04 18:00:55 -0400

    updated personal site (1 page)

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -29,7 +29,16 @@ module.exports = {
 			fontFamily: {
 				sans: ['Montserrat Variable', ...defaultTheme.fontFamily.sans],
 			},
+			screens: {
+				xs: '30rem',
+				sm: '40rem',
+				md: '48rem',
+				lg: '64rem',
+				xl: '80rem',
+				'2xl': '96rem',
+				'3xl': '120rem',
+			},
 		},
 	},
-	plugins: [],
+	plugins: [require('tailwindcss-logical')],
 };
