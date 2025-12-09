e6139abbe28a06b303431e37881fd32506e4860e
Max Lee <maxbmhlee@gmail.com>
2025-12-08 20:14:14 -0500

    remove svelte from deps (not really needed)

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,11 @@ import { defineConfig } from 'astro/config';
 
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import svelte from '@astrojs/svelte';
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [
-		svelte(),
 		tailwind({
 			applyBaseStyles: false,
 		}),
